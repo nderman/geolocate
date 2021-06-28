09045ab94650edc56b811f3532c75cd69eb25d6c
nderman <nderman@gmail.com>
2021-06-28 21:42:42 +0200

    generate code coverage report

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,8 @@ require File.expand_path("../config/environment", __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
+require "simplecov"
+SimpleCov.start
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -41,6 +43,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  config.formatter = :documentation
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
